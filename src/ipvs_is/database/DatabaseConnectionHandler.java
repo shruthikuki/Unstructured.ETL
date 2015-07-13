c9b159637b512c4844086dd16c1e56f9e76ff457
Shreyas BR <shreyasbr1991@gmail.com>
2015-07-13 22:46:49 +0200

    POS tagging

--- src/ipvs_is/database/DatabaseConnectionHandler.java
+++ src/ipvs_is/database/DatabaseConnectionHandler.java
@@ -1,6 +1,8 @@
 package ipvs_is.database;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
@@ -9,7 +11,7 @@ public class DatabaseConnectionHandler {
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 	static final String DB_URL = "jdbc:mysql://us-cdbr-iron-east-02.cleardb.net:3306/ad_97053fa1b7b08e0";
 
-	// Database credentials  
+	// Database credentials
 	static final String USER = "bd6c79c61fba4f";
 	static final String PASS = "9bcd28a7";
 
@@ -28,7 +30,7 @@ public class DatabaseConnectionHandler {
 			System.out.println("Creating statement...");
 			stmt = conn.createStatement();
 			String sql;
-			sql = "SELECT id, name FROM Trial";
+			sql = "SELECT * from POS_DATA";
 			ResultSet rs = stmt.executeQuery(sql);
 
 			// STEP 5: Extract data from result set
@@ -69,5 +71,123 @@ public class DatabaseConnectionHandler {
 		}// end try
 		System.out.println("Goodbye!");
 	}// end main
+
+	public String getDataSourceContent() {
+		Connection conn = null;
+		Statement stmt = null;
+		String dataSourceContent = null;
+		try {
+			// STEP 2: Register JDBC driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// STEP 3: Open a connection
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+			// STEP 4: Execute a query
+			System.out.println("Creating statement...");
+			stmt = conn.createStatement();
+			String sql;
+			sql = "SELECT * from DATA_SOURCE";
+			ResultSet rs = stmt.executeQuery(sql);
+
+			// STEP 5: Extract data from result set
+			while (rs.next()) {
+				// Retrieve by column name
+				dataSourceContent = rs.getString("content");
+
+				System.out.print("content: " + dataSourceContent);
+
+			}
+			// STEP 6: Clean-up environment
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		} finally {
+			// finally block used to close resources
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException se2) {
+			}// nothing we can do
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}// end finally try
+		}// end try
+		System.out.println("Goodbye!");
+		return dataSourceContent;
+
+	}
+
+	public ArrayList<String> getAllPOSData() {
+		HashMap<Integer, Integer> indexMap = new HashMap<Integer, Integer>();
+		ArrayList<String> posDataList = new ArrayList<String>();
+		Connection conn = null;
+		Statement stmt = null;
+		String dataSourceContent = null;
+		try {
+			// STEP 2: Register JDBC driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// STEP 3: Open a connection
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+			// STEP 4: Execute a query
+			System.out.println("Creating statement...");
+			stmt = conn.createStatement();
+			String sql;
+			sql = "SELECT BEGIN , END , TYPE from POS_DATA ORDER BY BEGIN DESC;";
+			ResultSet rs = stmt.executeQuery(sql);
+
+			// STEP 5: Extract data from result set
+			while (rs.next()) {
+				// Retrieve by column name
+				int begin = rs.getInt("begin");
+				int end = rs.getInt("end");
+				String posType = rs.getString("type");
+				System.out.print("begin: " + begin + " end: " + end + " pos: "
+						+ posType);
+				posDataList.add(String.valueOf(begin));
+				posDataList.add(String.valueOf(end));
+				posDataList.add(posType);
+			}
+			// STEP 6: Clean-up environment
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		} finally {
+			// finally block used to close resources
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException se2) {
+			}// nothing we can do
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}// end finally try
+		}// end try
+		System.out.println("Goodbye!");
+		return posDataList;
+
+	}
 
 }
--- src/ipvs_is/servlets/ResultGenerationServlet.java
+++ src/ipvs_is/servlets/ResultGenerationServlet.java
@@ -1,7 +1,14 @@
 package ipvs_is.servlets;
 
+import ipvs_is.database.DatabaseConnectionHandler;
+
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.TreeMap;
 
 import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
@@ -31,17 +38,54 @@ public class ResultGenerationServlet extends HttpServlet {
 	};
 
 	private void getResultForNER(ServletResponse response) {
-		String originalText;
+		DatabaseConnectionHandler databaseConnectionHandler = new DatabaseConnectionHandler();
+		StringBuilder originalText = new StringBuilder(
+				databaseConnectionHandler.getDataSourceContent());
+		HashMap<String, String> posColorMap = new HashMap<String, String>();
+		posColorMap.put("noun", "red");
+		posColorMap.put("verb", "green");
+		posColorMap.put("adjective", "blue");
 		// get all rows from NER table
 		// for each row
 		// using position of each token, in originalText, append appropriate
 		// divs
 
+		ArrayList<String> posDataList = databaseConnectionHandler
+				.getAllPOSData();
+
+		for (int i = 0; i < posDataList.size(); i++) {
+			int begin = Integer.parseInt(posDataList.get(i));
+			int end = Integer.parseInt(posDataList.get(i + 1));
+			String posType = posDataList.get(i + 2);
+			i = i + 2;
+			originalText.insert(end, "</font>");
+			originalText.insert(begin, " <font style = 'background-color:"
+					+ posColorMap.get(posType.toLowerCase()) + "'>");
+
+		}
+		System.out.println("highlighted text: " + originalText);
+
+		/*
+		 * ArrayList<Integer> beginIndexList = new
+		 * ArrayList<Integer>(indexMap.keySet()); ArrayList<Integer>
+		 * descendingBeginIndexList = Collections.sort(beginIndexList,
+		 * Collections.reverseOrder());
+		 */
+		/*
+		 * TreeMap<Integer, Integer> descendingIndexMap = new TreeMap<Integer,
+		 * Integer>( Collections.reverseOrder());
+		 * descendingIndexMap.putAll(indexMap); for (int begin :
+		 * descendingIndexMap.keySet()) { int end =
+		 * descendingIndexMap.get(begin);
+		 * 
+		 * }
+		 */
+
 		PrintWriter out = null;
 
 		try {
 			out = response.getWriter();
-			out.println("<div><b>Response for NER!!!</b></div>");
+			out.println(originalText);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
